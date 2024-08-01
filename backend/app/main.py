e176acafa9f5040280ffd8e3c79228c6fbaff9bf
Nart Madi <madirnart@gmail.com>
2024-08-01 08:27:18 +0300

    Add Flask for inference with endpoint for search

---
+++ backend/app/main.py
@@ -0,0 +1,27 @@
+from flask import request, jsonify, current_app as app, abort
+import pandas as pd
+import numpy as np
+import re
+import joblib
+from sklearn.metrics.pairwise import cosine_similarity
+
+tfidf = joblib.load("models/tfidf_vectorizer_eng.pkl")
+scaler = joblib.load("models/scaler.pkl")
+combined_features = np.load("models/combined_features.npy")
+df = pd.read_csv("models/books_data.csv")
+
+
+@app.route("/search", methods=["GET"])
+def search():
+    query = request.args.get("query")
+
+    if not query or query.isspace():
+        return jsonify({"error": "No search query provided"}), 400
+
+    query = re.sub(r"\W+", " ", query).strip().lower()
+
+    results = df[
+        df["title"].str.lower().str.contains(query, case=False, na=False, regex=False)
+    ]
+    search_results = results.to_dict(orient="records")
+    return jsonify(search_results)
